602560c369b52bc84c60a4c076e48fc6efc8d64c
Insleep <insleepdoesthings@gmail.com>
2017-08-14 13:05:27 -0500

    More docs

--- SparkWebBrowser/NSUserDefaults+ColorSupport.h
+++ SparkWebBrowser/NSUserDefaults+ColorSupport.h
@@ -10,7 +10,17 @@
 
 @interface NSUserDefaults(colorSupport)
 
+/**
+ * Set a color as the value of a user default.
+ * @param Color
+ * @param User default key
+ */
 - (void)setColor:(NSColor *)aColor forKey:(NSString *)aKey;
+
+/**
+ * Retrieve the color stored as a user default.
+ * @param User default key
+ */
 - (NSColor *)colorForKey:(NSString *)aKey;
 
 @end
